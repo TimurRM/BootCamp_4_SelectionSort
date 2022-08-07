abe88d06ba6411351afe41199fe093126aa7e8f4
Timur <tumyp.paxumob@gmail.com>
2022-08-07 23:09:25 +0500

    Added ControlWork_1

---
+++ ControlWork_1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите кол-во элементов (строк) массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] inputArray = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите {i + 1}-ю строку: ");
+    inputArray[i] = Console.ReadLine();
+}
+
+
+string myArray(string[] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            {
+            result += array[i]+ " ";
+            }
+    }
+    return result;
+}
+
+string[] outputArray = myArray(inputArray).Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine();
+Console.WriteLine("[" + string.Join(", ", inputArray) + "]" + " ==> " + "[" + string.Join(", ", outputArray) + "]");
+Console.WriteLine();
+Console.ReadKey();
+
