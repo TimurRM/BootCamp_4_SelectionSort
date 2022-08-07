ef985f2e10a60ef95f45dc386a65c707451a2a17
Timur <tumyp.paxumob@gmail.com>
2022-08-07 23:08:52 +0500

    Added Task_1 and Task_2

---
+++ Task_2.Sort_Vybor/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Введите кол-во элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+for (int i = 0; i < n; i++)
+
+
+{
+    Console.Write($"Введите {i+1}-e слово: ");
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "] -> ");
+
+
+for (int i = 0; i < n; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j].Length < array[MinIndex].Length)
+            MinIndex = j; 
+    }
+    string temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i]; 
+    array[i] = temp;
+}
+Console.WriteLine(" [" + string.Join(", ", array) + "]");
